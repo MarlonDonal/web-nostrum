8b4ac8739671f2a89a00c8288c5c206840e13b61
MarlonDonal <106673717+MarlonDonal@users.noreply.github.com>
2022-07-23 08:46:36 +0800

    Add files via upload

---
+++ Nostrum.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ 
+  <html>
+<?php
+    include("elements/head.php");
+?>    
+   <body>
+   <div id="loader" class="center">
+       
+  <?php
+        include("elements/header.php");
+        include("elements/content.php");
+        include("elements/footer.php");
+
+?>
+     
+   </body>
+       
+</html>
